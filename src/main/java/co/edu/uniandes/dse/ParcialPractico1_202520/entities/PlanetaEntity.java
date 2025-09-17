ed085e3a0ad09f8b6c5bc26514cb7c8a624b4fc7
Nicolas-loop <n.hernandez9@uniandes.edu.co>
2025-09-17 06:54:24 -0500

    Otro

--- src/main/java/co/edu/uniandes/dse/ParcialPractico1_202520/entities/PlanetaEntity.java
+++ src/main/java/co/edu/uniandes/dse/ParcialPractico1_202520/entities/PlanetaEntity.java
@@ -1,5 +1,20 @@
 package co.edu.uniandes.dse.ParcialPractico1_202520.entities;
 
+import jakarta.persistence.ManyToOne;
+import uk.co.jemos.podam.common.PodamExclude;
+import jakarta.persistence.Entity;
+
+
+
 public class PlanetaEntity {
     private String nombre;
+
+    private int poblacion;
+
+    private long diametro;
+
+    @PodamExclude
+    @ManyToOne
+    private SistemaSolarEntity sistemaSolar;
+
 }
--- src/main/java/co/edu/uniandes/dse/ParcialPractico1_202520/entities/SistemaSolar.java
+++ src/main/java/co/edu/uniandes/dse/ParcialPractico1_202520/entities/SistemaSolar.java
@@ -1,7 +1,25 @@
 package co.edu.uniandes.dse.ParcialPractico1_202520.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.OneToMany;
+import uk.co.jemos.podam.common.PodamExclude;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+
 public class SistemaSolar extends BaseEntity {
 
     private String nombre;
+    
+    private String region;
+
+    private long ratio_minimo;
+
+    private int num_stromtroopers;
+
+    @PodamExclude
+    @OneToMany(mappedBy = "sistema", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PlanetaEntity> planetas;
 
 }

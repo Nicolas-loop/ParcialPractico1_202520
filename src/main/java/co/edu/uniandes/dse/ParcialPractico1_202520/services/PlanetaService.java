e5e60417d11c3007ea85b542daadc099b934e03b
Nicolas-loop <n.hernandez9@uniandes.edu.co>
2025-09-17 07:18:39 -0500

    Algo falla

---
+++ src/main/java/co/edu/uniandes/dse/ParcialPractico1_202520/services/PlanetaService.java
@@ -0,0 +1,34 @@
+package co.edu.uniandes.dse.ParcialPractico1_202520.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.edu.uniandes.dse.ParcialPractico1_202520.entities.PlanetaEntity;
+import co.edu.uniandes.dse.ParcialPractico1_202520.entities.SistemaSolarEntity;
+import co.edu.uniandes.dse.ParcialPractico1_202520.exceptions.IllegalOperationException;
+import co.edu.uniandes.dse.ParcialPractico1_202520.repositories.PlanetaRepository;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class PlanetaService {
+    @Autowired
+    private PlanetaRepository planetaRepository;
+
+    @Autowired
+    private SistemaSolarEntity sistemaSolarEntity;
+
+    private static final String[] regionesValidas = {"CORE", "MID_RIM", "OUTER_RIM", "COLONIES", "WILD_SPACE"};
+
+    @Transactional
+    public PlanetaEntity createPlaneta(PlanetaEntity planetaEntity) throws IllegalOperationException{
+        log.info("Inicia proceso de creación del planeta");
+
+        //Verificar la longitud del nombre
+        if (planetaEntity.getNombre())
+    }
+}
